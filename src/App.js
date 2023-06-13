32837f2afbbbf5d7dfece5ed709815aecd8592ed
Andrii Motko <74198406+AndriiMotko@users.noreply.github.com>
2023-06-13 10:05:26 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import RegexPage from "./pages/regex";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <RegexPage />
+    </div>
+  );
+}
---
+++ src/pages/regex/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const DIV = styled.div``;
+
+export const PageContainer = styled.div`
+  padding: 1rem;
+  margin: 0;
+`;
+
+export const H1 = styled.h1``;
+export const H2 = styled.h2``;
+export const H3 = styled.h3``;
+export const H4 = styled.h4``;
+export const H5 = styled.h5``;
+
+export const OL = styled.ol``;
+export const LI = styled.li``;
+
+
+export const Def = styled.p``;
+export const Text = styled.p``;
+
+export const Code = styled.code`
+  display: inline-flex;
+  background-color: rgba(52, 53, 65, 1);
+  padding: 0.25rem 0.75rem;
+  border: 1px solid rgba(52, 53, 65, 1);
+  border-radius: 0.5rem;
+  font-family: "Courier New", monospace;
+  color: #FCE22A;
+`;
