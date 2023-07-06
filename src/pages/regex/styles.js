ca350fa3c2050841c0d1541d7388aabb23dccc7d
Andrii Motko <74198406+AndriiMotko@users.noreply.github.com>
2023-07-06 09:43:50 +0000

    Update 5 files

--- src/pages/regex/styles.js
+++ src/pages/regex/styles.js
@@ -3,19 +3,32 @@ import styled from 'styled-components';
 export const DIV = styled.div``;
 
 export const PageContainer = styled.div`
-  padding: 1rem;
-  margin: 0;
+  padding: 0 1rem;
+  margin: 0 auto;
+  max-width: 1024px;
+  background: rgba(117, 115, 115, 0.11);
+  box-shadow: 10px 0 5px -5px rgba(0, 0, 0, 0.15), -10px 0 5px -5px rgba(0, 0, 0, 0.15);
 `;
 
-export const H1 = styled.h1``;
+export const H1 = styled.h1`
+  padding: 2rem 0;
+`;
 export const H2 = styled.h2``;
 export const H3 = styled.h3``;
 export const H4 = styled.h4``;
 export const H5 = styled.h5``;
 
-export const OL = styled.ol``;
+export const OL = styled.ol`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`;
 export const UL = styled.ul`
-  list-style-type: square;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin: 0.5rem 0;
+  list-style-type: ${(pr) => pr?.listStyle || 'square'};
 `;
 export const LI = styled.li``;
 
